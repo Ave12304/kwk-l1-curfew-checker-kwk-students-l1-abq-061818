49f536cb49e9afbe5af59d2eb22429a178055e9a
Ave12304 <averyselover@hotmail.com>
2018-06-20 15:22:10 +0000

    automatically backed up by learn

--- lib/curfew_checker.rb
+++ lib/curfew_checker.rb
@@ -1,19 +1,8 @@
-def simple_curfew_checker(time)
-  # code goes here
+def simple_curfew_checker(11:00)
+  if "its_11:00 "
+  puts "go_to_bed"  
+else "stay out"
+end
 end
 
-def curfew_checker(time)
-  # code goes here
-end
-
-def complex_curfew_checker(time)
-  # code goes here
-end
-
-def deluxe_curfew_checker(time)
-  # code goes here
-end
-
-def platinum_curfew_checker(current_time, curfew_time)
-  # code goes here
-end
+def 
